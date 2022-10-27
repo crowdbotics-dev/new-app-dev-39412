f04d265d687d034a6372c05a37795b3c3cc79355
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-10-27 16:49:42 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
+const styles = StyleSheet.create({});
 import { StyleSheet } from "react-native";
-
 export const globalOptions = {
-  name: "new_app_dev_39412",
-  url: "https://new_app_dev_39412.botics.co",
-  api: "https://new_app_dev_39412.botics.co/api/v1"
-}
+  "name": "new_app_dev_39412",
+  "url": "https://new_app_dev_39412.botics.co",
+  "api": "https://new_app_dev_39412.botics.co/api/v1"
+};
 export const modulesOptions = {
-  "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
-}
+  "@modules/app-menu": {}
+};
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.HzAiYiFr}>
+      </View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  HzAiYiFr: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
